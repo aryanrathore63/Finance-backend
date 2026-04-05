3646b8726e49fae49a904799075c543698afd4af
Aryan Rathore <aryan.12114529@lpu.in>
2026-04-05 14:21:06 +0530

    Enable Swagger UI in all environments including Production

--- FinanceBackend/Program.cs
+++ FinanceBackend/Program.cs
@@ -83,11 +83,8 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseMiddleware<ErrorHandlingMiddleware>();
 
